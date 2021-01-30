557a06dfa10c70e29c167b1ada70e55b572809e0
Coline <coline.seguret@epitech.eu>
2021-01-30 15:41:24 +0100

    [ADD] the quality of ...

--- parse/metaData.py
+++ parse/metaData.py
@@ -18,6 +18,11 @@ for session_file in os.listdir(REPO):
     line += isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))
     line += isNan(re.findall("Roll number .+?(?= Date)", data))
     line += isNan(re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data))
+    line += isNan(re.findall("(?:THE QUALITY OF PUPILS’|the quality of pupils’ ).+?(?=[0-9])", data))
+    line += isNan(re.findall("(?:THE QUALITY OF TEACHING|the quality of teaching ).+?(?=[0-9])", data))
+    line += isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))
+    line += isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))
+    line += isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))
     parsed.append(line)
 
 for line in parsed:
