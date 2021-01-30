537e31a93ae55c1f9a87f4456df77f462d8b60cc
Coline <coline.seguret@epitech.eu>
2021-01-30 12:59:54 +0100

    [ADD] NaN if information not find

--- parse/metaData.py
+++ parse/metaData.py
@@ -2,8 +2,14 @@ import pandas as pd
 import os
 import re
 
-REPO = "plain_text"
+REPO = "Reports/plain_text"
 parsed = []
+
+def isNan (_str):
+    if not _str:
+        return ["Nan"]
+    else:
+        return _str
 
 for session_file in os.listdir(REPO):
     filepath = os.path.join(REPO, session_file)
@@ -11,10 +17,10 @@ for session_file in os.listdir(REPO):
     f = open(filepath)
     data = f.read()
     line = []
-    line += re.findall("School name .+?(?= School| Seoladh)", data)
-    line += re.findall("School address .+?(?= Uimhir| Roll)", data)
-    line += re.findall("Roll number .+?(?= Date)", data)
-    line += re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data)
+    line += isNan(re.findall("School name .+?(?= School| Seoladh)", data))
+    line += isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))
+    line += isNan(re.findall("Roll number .+?(?= Date)", data))
+    line += isNan(re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data))
     parsed.append(line)
 
 for line in parsed:
