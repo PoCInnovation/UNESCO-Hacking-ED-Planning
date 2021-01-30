8de0915ce129e9e4302f7239dc09488f84267a2a
Tom Chauveau <tom.chauveau@epitech.eu>
2021-01-30 13:25:35 +0100

    refactor(mock-up): simplify code

--- mock-up-generator.py
+++ mock-up-generator.py
@@ -1,77 +1,8 @@
 import sys
-import json
+import csv, json
 import random
 
 from typing import List
-
-
-def get_file(filename: str) -> str:
-    """
-    Get file content from given filename
-
-    :param filename: name of the file
-    :exception: File not found
-    :return: file content
-    """
-    try:
-        with open(filename, "r", encoding="latin-1") as file:
-            file_content = file.read()
-            file.close()
-    except FileNotFoundError:
-        print("ERROR: file doesn't exist")
-        exit(84)
-    return file_content
-
-
-def add_mockup(content: List[dict]) -> None:
-    """
-    Add a field `note` with a random number between 0 and 10
-
-    :param content: List of json object
-    """
-    ""
-    rnd = random.Random()
-    notes = [i for i in range(10)]
-
-    for school in content:
-        school["note"] = rnd.choice(notes)
-
-
-def extract_fields_from_file(file: str) -> List[dict]:
-    """
-    CSV Supported : ['Roll_No', 'Off_Name', 'County', 'Ethos', 'xcoord', 'ycoord', 'Long', 'Lat']
-
-    Algo :
-        - Split array by line and by column
-        - Transform all line into well formatted json
-        - Push it to an List
-    :param file: csv data
-    :return: List of JSON with formatted fields
-    """
-    content = file.replace('"', '').split('\n')
-    fields = [line.split(',') for line in content]
-    legend = fields[0]
-    schools = fields[1:]
-
-    schools_dto: List[dict] = []
-    for school in schools:
-        if len(school) != len(legend):
-            continue
-        school_json = {}
-        for i in range(len(legend)):
-            school_json[legend[i]] = school[i]
-        schools_dto.append(school_json)
-    return schools_dto
-
-
-def save_result(fields: List[dict]) -> None:
-    with open("result.json", "w") as file:
-        file.write('{\n"data": [')
-        for school in fields:
-            file.write(json.dumps(school, indent=4))
-            file.write(',\n')
-        file.write('{}\n]\n}')
-        file.close()
 
 
 def display_help() -> None:
@@ -79,25 +10,28 @@ def display_help() -> None:
     print("CSV ANALYZER")
     print("Options:")
     print("-m : create a random note column to mock result")
-    print("-s : save the result into result.json file")
     print("Usage:")
-    print("python3 mock-up-generator.py <file to parse> <options>")
+    print("python3 mock-up-generator.py <file to parse> <output file> <options>")
 
 
 def main(ac: int, av: List[str]) -> None:
-    if ac == 0:
+    if ac < 2:
         display_help()
         exit(84)
-    filename = av[0]
-    file = get_file(filename)
-    fields = extract_fields_from_file(file)
-    if "-m" in av:
-        add_mockup(fields)
-    if "-s" in av:
-        save_result(fields)
-    else:
-        for school in fields:
-            print(json.dumps(school, indent=4))
+
+    data = {}
+    with open(av[0]) as csvFile:
+        rnd = random.Random()
+        notes = [i for i in range(10)]
+
+        csv_reader = csv.DictReader(csvFile)
+        for rows in csv_reader:
+            if "-m" in av:
+                rows["note"] = str(rnd.choice(notes))
+            data[rows["Roll_No"]] = rows
+
+    with open(av[1], "w") as jsonFile:
+        jsonFile.write(json.dumps(data, indent=4))
 
 
 if __name__ == '__main__':
