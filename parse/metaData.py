57659057d9fe41ab8066a3906b94dd9d682965d7
Thytu <valentin.de-matos@epitech.eu>
2021-01-30 23:36:10 +0100

    *data: metadata increase leader ship success rate from 7% to 60%

--- parse/metaData.py
+++ parse/metaData.py
@@ -21,10 +21,24 @@ def getInfo (path):
     line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF PUPILS’|the quality of pupils’ ).+?(?=[0-9])", data))[0]))
     line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF TEACHING|the quality of teaching ).+?(?=[0-9])", data))[0]))
     line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))[0]))
-    line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))[0]))
+    seven = re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data, re.IGNORECASE))[0])
+    if seven == 'NaN': seven = re.sub(r'\W+ ', '', isNan(re.findall("(QUALITY OF SCHOOL MANAGEMENT AND LEADERSHIP.+?)QUALITY OF LEARNING AND TEACHING", data, flags=re.IGNORECASE|re.DOTALL))[0])
+    line.append(seven)
     line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))[0]))
     return (line)
+    # if line[7] == 'NaN':
+    #     print("../Reports/pdf/" + path[len("../Reports/plain_text/"):-len(".txt")] + ".pdf")
+    #     exit()
+    # return (line)
 
 
 if __name__ == '__main__':
-    print (getInfo("../Reports/plain_text/13299E_13_01_2021.txt")[4])
+    res = []
+    for f in os.listdir("../Reports/plain_text/"):
+        res.append(getInfo("../Reports/plain_text/" + f))
+    missing = 0
+    for r in res:
+        if r[7] == 'NaN':
+            missing += 1
+    print("Success rate:", 100 - missing / len(res) * 100)
+    # print (getInfo("../Reports/plain_text/13299E_13_01_2021.txt")[4])
