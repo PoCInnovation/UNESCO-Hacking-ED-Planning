d8d5ae0edca1cfcf677d9217f5939baccdafcb0f
Thytu <valentin.de-matos@epitech.eu>
2021-01-30 20:56:28 +0100

    *data: take only alphanum during regex

--- parse/metaData.py
+++ parse/metaData.py
@@ -14,37 +14,16 @@ def getInfo (path):
     data = f.read()
     line = []
 
-    line += isNan(re.findall("School name .+?(?= School| Seoladh)", data))
-    line += isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))
-    line += isNan(re.findall("Roll number .+?(?= Date)", data))
-    line += isNan(re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data))
-    line += isNan(re.findall("(?:THE QUALITY OF PUPILS’|the quality of pupils’ ).+?(?=[0-9])", data))
-    line += isNan(re.findall("(?:THE QUALITY OF TEACHING|the quality of teaching ).+?(?=[0-9])", data))
-    line += isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))
-    line += isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))
-    line += isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("School name .+?(?= School| Seoladh)", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("Roll number .+?(?= Date)", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF PUPILS’|the quality of pupils’ ).+?(?=[0-9])", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF TEACHING|the quality of teaching ).+?(?=[0-9])", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))[0]))
+    line.append(re.sub(r'\W+ ', '', isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))[0]))
     return (line)
-
-
-#for session_file in os.listdir(REPO):
-#    filepath = os.path.join(REPO, session_file)
-#
-#    f = open(filepath)
-#    data = f.read()
-#    line = []
-#    line += isNan(re.findall("School name .+?(?= School| Seoladh)", data))
-#    line += isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))
-#    line += isNan(re.findall("Roll number .+?(?= Date)", data))
-#    line += isNan(re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data))
-#    line += isNan(re.findall("(?:THE QUALITY OF PUPILS’|the quality of pupils’ ).+?(?=[0-9])", data))
-#    line += isNan(re.findall("(?:THE QUALITY OF TEACHING|the quality of teaching ).+?(?=[0-9])", data))
-#    line += isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))
-#    line += isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))
-#    line += isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))
-#    parsed.append(line)
-#
-#for line in parsed:
-#    print(line)
 
 if __name__ == '__main__':
     path = sys.argv[1]
