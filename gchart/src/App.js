f74c304f5bbf2ee4d8c198d648e30e0f6207fbb5
PtitLuca <luca.georges-francois@epitech.eu>
2021-01-30 16:42:49 +0100

    [+] - Add chart.

--- gchart/src/App.js
+++ gchart/src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
 import Chart from "react-google-charts";
+import dotenv from "dotenv";
+import data from "./out.json";
 
+dotenv.config();
+const regions = [];
+Object.keys(data).forEach((key) => {
+  console.log(data[key]);
+})
 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
 function App() {
   return (
-    <div className="App">
-      <Chart
-        width={window.innerWidth}
-        height={window.innerHeight}
-        chartType="GeoChart"
-        data={[
-          ['City', 'Population', 'Area'],
-          ['Dublin', 2761477, 1285.31],
-        ]}
-        options={{
-          backgroundColor: '#D6EAF8',
-          region: 'IE',
-          displayMode: 'markers',
-          resolution: 'provinces',
-          colorAxis: { colors: ['green', 'blue'] },
-        }}
-        mapsApiKey="YOUR_KEY_HERE"
-        rootProps={{ 'data-testid': '2' }}
-      />
-    </div>
+    /*
+    <Chart
+      width={'1000px'}
+      height={'700px'}
+      chartType="GeoChart"
+      data={[
+        ['City', 'School Name', 'Note'],
+        ['Carlow', 'BALLYCONNELL N S', 4],
+        ['Carlow', 'BORRIS MXD N S', 9],
+        ['Clare', 'S N MHUIRE MILIUC', 3],
+      ]}
+      options={{
+        region: 'IE',
+        displayMode: 'markers',
+        resolution: 'provinces',
+        colorAxis: { colors: ['green', 'blue'] },
+      }}
+      mapsApiKey={process.env.REACT_APP_GCP_API_KEY}
+      rootProps={{ 'data-testid': '2' }}
+    />
+    */
+   <div>
+     PtitLuca
+   </div>
   );
 }
 
