2fcc59b57d879366f884d8adccabb236bb2fd71f
Coline <coline.seguret@epitech.eu>
2021-01-30 20:02:56 +0100

    [ADD] function to get info in txt

--- parse/metaData.py
+++ parse/metaData.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import re
+import sys
 
 REPO = "Reports/plain_text"
 parsed = []
@@ -8,12 +9,11 @@ parsed = []
 def isNan (_str):
     return _str if _str else ["NaN"]
 
-for session_file in os.listdir(REPO):
-    filepath = os.path.join(REPO, session_file)
-
-    f = open(filepath)
+def getInfo (path):
+    f = open(path)
     data = f.read()
     line = []
+
     line += isNan(re.findall("School name .+?(?= School| Seoladh)", data))
     line += isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))
     line += isNan(re.findall("Roll number .+?(?= Date)", data))
@@ -23,7 +23,29 @@ for session_file in os.listdir(REPO):
     line += isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))
     line += isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))
     line += isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))
-    parsed.append(line)
+    return (line)
 
-for line in parsed:
-    print(line)
+
+#for session_file in os.listdir(REPO):
+#    filepath = os.path.join(REPO, session_file)
+#
+#    f = open(filepath)
+#    data = f.read()
+#    line = []
+#    line += isNan(re.findall("School name .+?(?= School| Seoladh)", data))
+#    line += isNan(re.findall("School address .+?(?= Uimhir| Roll)", data))
+#    line += isNan(re.findall("Roll number .+?(?= Date)", data))
+#    line += isNan(re.findall("Date of (?:Evaluation:|inspection:) .+?(?= )", data))
+#    line += isNan(re.findall("(?:THE QUALITY OF PUPILS’|the quality of pupils’ ).+?(?=[0-9])", data))
+#    line += isNan(re.findall("(?:THE QUALITY OF TEACHING|the quality of teaching ).+?(?=[0-9])", data))
+#    line += isNan(re.findall("(?:THE QUALITY OF SUPPORT|the quality of support ).+?(?=[0-9])", data))
+#    line += isNan(re.findall("(?:THE QUALITY OF LEADERSHIP|the quality of LEADERSHIP ).+?(?=[0-9])", data))
+#    line += isNan(re.findall("(?:THE QUALITY OF SCHOOL|the quality of school ).+?(?=[0-9])", data))
+#    parsed.append(line)
+#
+#for line in parsed:
+#    print(line)
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    print (getInfo(path))
